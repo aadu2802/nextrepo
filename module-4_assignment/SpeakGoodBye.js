d8b94aae786027c017fa09adadfabb37d30a0d51
aadu2802 <aadu2802@gmail.com>
2020-05-06 12:29:39 +0530

    4-mod

---
+++ module-4_assignment/SpeakGoodBye.js
@@ -0,0 +1,5 @@
+var byeSpeaker = {};
+byeSpeaker.name = "Good Bye";
+byeSpeaker.speak=function(name){
+	console.log("Good Bye" + " " +name);
+}
---
+++ module-4_assignment/SpeakHello.js
@@ -0,0 +1,5 @@
+var helloSpeaker = {};
+helloSpeaker.name = "Hello";
+helloSpeaker.speak= function(name){
+	console.log("Hello " + " " +name);
+}
